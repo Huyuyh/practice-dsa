19cbc9e74db58802e92f363896ed8d18ae7f2bb8
huyuyh <phamhuy09042002@gmail.com>
2026-01-27 17:20:49 -0500

    refactor

---
+++ leetcode/stack/valid_parenthese/valid_parenthese_2.js
@@ -0,0 +1,35 @@
+/**
+ * @param {string} str
+ * @return {boolean}
+ */
+export default function isBalancedBrackets(str) {
+  const array = str.split("");
+  const map = {
+    ")": "(",
+    "}": "{",
+    "]": "[",
+  };
+
+  var stack = [];
+
+  for (var i = 0; i < array.length; i++) {
+    const item = array[i];
+
+    if (item === "(" || item === "{" || item === "[") {
+      stack.push(item);
+    } else {
+      const mapItem = map[item];
+      const topItem = stack[stack.length - 1];
+
+      if (topItem === mapItem) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0;
+}
+
+console.log(isBalancedBrackets("})"));
